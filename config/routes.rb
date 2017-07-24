cb2964ecdb4e398f4da6ea465886ec234bb9fe6d
Ebrahim Khalilullah <eksumon769@gmail.com>
2017-07-24 08:05:31 +0600

    API Working with endpoint GET /ambulances . Unit test added. Authetication Added

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  put "/admins", to: "admins#update"
+  patch "/admins", to: "admins#update"
+  resources :ambulances
+  resources :infos
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
